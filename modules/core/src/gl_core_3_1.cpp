c1a653793854c5efbfa109ca6081a61b51a883f4
Oli Wilkie <oliverwilkie@gmail.com>
2013-12-19 23:48:32 +0000

    Unknown Symbol String error if compiling on OS X. Replaced String with std::string and compiles ok.

--- modules/core/src/gl_core_3_1.cpp
+++ modules/core/src/gl_core_3_1.cpp
@@ -54,8 +54,8 @@
                 image = NSAddImage("/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL", NSADDIMAGE_OPTION_RETURN_ON_ERROR);
 
             // prepend a '_' for the Unix C symbol mangling convention
-            String symbolName = "_";
-            symbolName += String(name);
+            std::string symbolName = "_";
+            symbolName += std::string(name);
 
             NSSymbol symbol = image ? NSLookupSymbolInImage(image, &symbolName[0], NSLOOKUPSYMBOLINIMAGE_OPTION_BIND | NSLOOKUPSYMBOLINIMAGE_OPTION_RETURN_ON_ERROR) : 0;
 
